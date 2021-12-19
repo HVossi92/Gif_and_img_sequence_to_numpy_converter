c8496c2baaaa5b8994f52aebe5c08783452444b3
Vossi <vossi@pop-os.localdomain>
2021-12-19 15:43:37 +0100

    Create numpy converter

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from itertools import cycle, islice
+from pathlib import Path
+import cv2
+import imageio as iio
+import numpy as np
+from natsort import natsort
+
+# All image files get converted to the same width and height, else it would create a rugged array
+image_dimensions = (64, 64)
+# All image sequences get the same length, else it creates a rugged array. Longer sequences just get cut off,
+# shorter sequences get repeated
+num_frames = 150
+# All GIFs inside this directory get combined into one numpy array
+directory_path = '/home/vossi/Documents/Master_Thesis/WebScraping/Scraped_Data/Image_Sequences/'
+# Output file will be a numpy array .npy file
+output_file = "/home/vossi/Documents/Master_Thesis/WebScraping/Scraped_Data/output_array"
+
+
+def convert():
+    directory_contents = os.listdir(directory_path)
+
+    dataset = []
+    for idx, item in enumerate(directory_contents):
+        if os.path.isdir(directory_path + item):
+            print(f"Reading GIF {idx} / {len(directory_contents)}")
+            gif_list = []
+            cur_frame = 0
+            files_in_path = Path(directory_path + item).iterdir()
+            for file in natsort.natsorted(files_in_path):
+                # print(file)
+                im = iio.imread(file)
+                # Stop if GIF longer than 150 frames
+                if cur_frame >= num_frames:
+                    break
+                resized_frame = cv2.resize(im, dsize=image_dimensions, interpolation=cv2.INTER_CUBIC)
+                gif_list.append(resized_frame)
+                cur_frame += 1
+
+                # Repeat the GIF, if shorter than 150 frames
+            if cur_frame < num_frames:
+                gif_list = list(islice(cycle(gif_list), num_frames))
+            dataset.append(gif_list)
+
+    # Turn list into numpy array
+    dataset = np.array(dataset)
+    for i in range(len(dataset)):
+        dataset[i] = np.array(dataset[i])
+
+    np.save(output_file, dataset, allow_pickle=True, fix_imports=True)
+    print(f"Created numpy array of shape: {dataset.shape}")
+
+
+if __name__ == '__main__':
+    convert()
